6dcb5706e471545268a15003f11dd0d68ca2d39a
Berliana Novianti <berliananovianti43@gmail.com>
2024-03-09 19:21:41 +0700

    merubah program membaca memo yg sblmnya

--- 231511072/231511072.cpp
+++ 231511072/231511072.cpp
@@ -1,22 +1,46 @@
-#include "231511072.h"
-#include <fstream>
-#include <iostream>
-using namespace std;
+//#include "231511072.h"
+// #include <fstream>
+// #include <iostream>
+// using namespace std;
 
-void readMemoFile(const char* filename, Memo& memo) {
-    ifstream file(filename, binary);
-    if (!file.is_open()) {
-        cerr << "Gagal Membuka Memo: " << filename << endl;
-        return;
-    }
-
+// void readMemoFile(const char* filename, Memo& memo) {
+    // ifstream file(filename, binary);
+    // if (!file.is_open()) {
+        // cerr << "Gagal Membuka Memo: " << filename << endl;
+        // return;
+    //}
     // Read memo name
-    getline(file, memo.namaMemo);
+    // getline(file, memo.namaMemo);
 
     // Read date (assumed to be the next line)
-    getline(file, memo.tanggal);
+    // getline(file, memo.tanggal);
 
-    getline(file, memo.isiMemo); 
+    // getline(file, memo.isiMemo); 
 
-    file.close();
+    // file.close();
+//} 
+// memo_reader.cpp
+#include <iostream>
+#include <fstream>
+#include "231511072.h"
+
+void readMemoFile(const char* filename, Memo& memo) {
+    ifstream file(filename, ios::binary);
+
+    if (file.is_open()) {
+        file.read(reinterpret_cast<char*>(&memo), sizeof(Memo));
+        file.close();
+
+        cout << "Nama Memo: " << memo.namaMemo << endl;
+        cout << "Tanggal: " << memo.tanggal << endl;
+        cout << "Isi Memo: " << memo.isiMemo << endl;
+    } else {
+        cerr << "Gagal membuka file " << filename << endl;
+    }
+}
+
+int main() {
+    Memo memo;
+    readMemoFile("memo.dat", memo);
+    return 0;
 }
