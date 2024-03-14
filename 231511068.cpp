ef0427711f068307c4375515aa4524620f1ad7e4
Alqan Nazra <alqan.nazra.tif23@polban.ac.id>
2024-03-14 16:49:07 +0700

    .

--- 231511068.cpp
+++ 231511068.cpp
@@ -1,7 +1,4 @@
-#include <stdio.h>
-#include <math.h>
-#include <string.h>
-#include <stdlib.h>
+#include "231511068.h"
 /*
 P = Banyak  Bilangan [Harus prima saat diimput]
 Q = Banyak Bilangan [Harus prima saat diimput]
@@ -17,11 +14,10 @@ Peraturan Definisi
 Array input itu Array [1][j]
 */
 
- long int p, q, n, t, cekinputprima, e[100][100], d[100][100], temp[100][100], j, pass[100][100], enkripsi[100][100], i;
- char password[100][100];
+  long int p, q, n, t, cekinputprima, e[100][100], d[100][100], temp[100][100], j, pass[100][100], enkripsi[100][100], i;
+  char password[100][100];
 
- 
-int cekprim (long int prima)
+  int cekprim (long int prima)
 {
     int i;
     long int j = sqrt(prima);
@@ -34,33 +30,103 @@ int cekprim (long int prima)
     }
     return 1;
 }
- int main ()
- {
-    printf ("Angka dengan minimal diatas dari 2\n");
-    printf ("Masukkan Angka Prima Pertama: ");
-    scanf ("%ld",&p);
-    cekinputprima = cekprim(p);
-    if (cekinputprima == 0)
+
+void bagE (long int cekinputprima)
+{
+    int k,l;
+    k = 0;
+    l = 0;
+    for (i = 2; i < t;i++)
     {
-        printf ("Input mengalami error// tidak prima");
-        exit(1);
+        // if (t % i == 0) /*Task ini digunakan untuk melanjutkan loop dari yang diinginkan agar 0 tidak dimasukkan pada saat nanti operasi array*/ 
+        // {
+        //     continue;
+        // }
+        cekinputprima = cekprim(i);
+        if (cekinputprima == 1 && i != p && i != q)
+        {
+            e[k][l] = i;
+            cekinputprima = bagianD(enkripsi[k][l]);
+            if (cekinputprima > 0)
+            {
+                d[k][l] = cekinputprima;
+                k++;
+            }
+        }
+        
+
     }
-    printf ("Angka dengan minimal diatas dari 1\n");
-    printf ("Masukkan angka Prima kedua: ");
-    scanf ("%ld",&q);
-    cekinputprima = cekprim(q);
-    if (cekinputprima == 0 || p == q)
-    {
-        printf ("Input mengalami error/ TIdak prima");
-        exit (1);
-    }
-    printf ("Masukkan Password yang akan digunakan");
-    fflush(stdin);
-    scanf("%s",password);
-    
-
-
-
 }
+
+long int bagianD(long int x) // Membuat  modular D
+{
+    long int k = 1;   
+    while (1)
+    {
+        k = k + t;
+        if (k % x ==0)
+        {
+            return (k/x);
+        }
+    }
+}
+
+
+void encrypt(long int pass[][100])
+{
+    long int pastukar, ct, kunci = e[0][0], k,len;
+    int i = 0, j = 0;
+    int panstring = strlen(password[0]); // Panjang string pada posisi pada array password[] merupakan posisi baris array
+    while (i != panstring) 
+    {
+        pastukar = password[i][j];
+        pastukar = pastukar - 96;
+        k = 1;
+        for (j = 0; j < kunci; j++) {
+            k = k * pastukar;
+            k = k % n;
+        }
+        temp[i][j] = k;
+        ct = k + 96;
+        enkripsi[i][j] = ct;
+        i++;
+    }
+    enkripsi[1][0]= -1;
+    printf ("\n Password sudah dienkripsi");
+//     for (i = 0; enkripsi[i][0] != -1; i++) // Menggunakan en[i][0] != -1 sebagai kondisi loop
+//         printf("%c", enkripsi[i][0]);
+}
+ 
+
+
+
+//  int main ()
+//  {
+//     printf ("Angka dengan minimal diatas dari 2\n");
+//     printf ("Masukkan Angka Prima Pertama: ");
+//     scanf ("%ld",&p);
+//     cekinputprima = cekprim(p);
+//     if (cekinputprima == 0)
+//     {
+//         printf ("Input mengalami error// tidak prima");
+//         exit(1);
+//     }
+//     printf ("Angka dengan minimal diatas dari 1\n");
+//     printf ("Masukkan angka Prima kedua: ");
+//     scanf ("%ld",&q);
+//     cekinputprima = cekprim(q);
+//     if (cekinputprima == 0 || p == q)
+//     {
+//         printf ("Input mengalami error/ TIdak prima");
+//         exit (1);
+//     }
+//     printf ("Masukkan Password yang akan digunakan");
+//     fflush(stdin);
+//     scanf("%s",password);
+
+
+
+
+// }
 
 
