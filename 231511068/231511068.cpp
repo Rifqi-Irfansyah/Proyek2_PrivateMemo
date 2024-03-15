c3481d0dc82f3d959de05a15c14f992842b9f2b5
Alqan Nazra <alqan.nazra.tif23@polban.ac.id>
2024-03-15 20:40:34 +0700

    Konversi array 2D ke 1D next modul target :v

--- 231511068/231511068.cpp
+++ 231511068/231511068.cpp
@@ -1,4 +1,8 @@
 #include "231511068.h"
+
+typedef long int satuan;
+typedef int loop;
+typedef int array;
 /*
 P = Banyak  Bilangan [Harus prima saat diimput]
 Q = Banyak Bilangan [Harus prima saat diimput]
@@ -14,57 +18,64 @@ Peraturan Definisi
 Array input itu Array [1][j]
 */
 
-  long int p, q, n, t, cekinputprima, e[100][100], d[100][100], temp[100][100], j, pass[100][100], enkripsi[100][100], i;
+  satuan p, q, n, t, cek,x;
+  loop i,j;
+  array e[100], d[100], temp[100][100],pass[100][100], enkripsi[100][100];
   char password[100][100];
 
-  int cekprim (long int prima)
-{
+int cekprim( int prima) {
     int i;
-    long int j = sqrt(prima);
-    for (i = 2; i <= j; i++)
-    {
-        if (prima % i == 0)
-        {
+    int j = sqrt(prima);
+    for (i = 2; i <= j; i++) {
+        if (prima % i == 0) {
             return 0;
         }
     }
     return 1;
 }
 
-void bagE (long int cekinputprima)
+int mengolahtdann(int p, int q) {
+    n = p * q;
+    t = (p - 1) * (q - 1);
+    printf("Nilai t: %d\n", t);
+    printf("Nilai n: %d\n", n);
+    return 0; // Menambahkan nilai kembalian karena fungsi ini memiliki tipe int
+}
+
+void bagE (int t,int *e,int *d) 
 {
-    int k,l;
+    int k;
     k = 0;
-    l = 0;
     for (i = 2; i < t;i++)
     {
-        // if (t % i == 0) /*Task ini digunakan untuk melanjutkan loop dari yang diinginkan agar 0 tidak dimasukkan pada saat nanti operasi array*/ 
-        // {
-        //     continue;
-        // }
-        cekinputprima = cekprim(i);
-        if (cekinputprima == 1 && i != p && i != q)
+        if (t % i == 0) /*Task ini digunakan untuk melanjutkan loop dari yang diinginkan agar 0 tidak dimasukkan pada saat nanti operasi array*/ 
+            continue;
+        cek = cekprim(i);
+        if (cek == 1 && i != p && i != q)
         {
-            e[k][l] = i;
-            cekinputprima = bagianD(enkripsi[k][l]);
-            if (cekinputprima > 0)
+            e[k]= i;
+            cek = bagianD(e[k]);
+            if (cek > 0)
             {
-                d[k][l] = cekinputprima;
+                d[k] = cek;
                 k++;
             }
+                if (k == 99)
+                break;
         }
         
-
+        cout << "____"<<k;
+        cout << "----"<<cek;
     }
 }
 
-long int bagianD(long int x) // Membuat  modular D
+int bagianD(int t) // Membuat  modular D
 {
-    long int k = 1;   
+    int k = 1;   
     while (1)
     {
         k = k + t;
-        if (k % x ==0)
+        if (k % x == 0)
         {
             return (k/x);
         }
@@ -72,18 +83,18 @@ long int bagianD(long int x) // Membuat  modular D
 }
 
 
-void encrypt(long int pass[][100])
+void encrypt()
 {
-    long int pastukar, ct, kunci = e[0][0], k,len;
+    int Op, ct, kunci = e[0], k,panjang;
     int i = 0, j = 0;
-    int panstring = strlen(password[0]); // Panjang string pada posisi pada array password[] merupakan posisi baris array
-    while (i != panstring) 
+    panjang = strlen(password[0]); // Panjang string pada posisi pada array password[] merupakan posisi baris array
+    while (i != panjang) 
     {
-        pastukar = password[i][j];
-        pastukar = pastukar - 96;
+        Op = password[i][j];
+        Op = Op - 96;
         k = 1;
         for (j = 0; j < kunci; j++) {
-            k = k * pastukar;
+            k = k * Op;
             k = k % n;
         }
         temp[i][j] = k;
@@ -93,40 +104,66 @@ void encrypt(long int pass[][100])
     }
     enkripsi[1][0]= -1;
     printf ("\n Password sudah dienkripsi");
-//     for (i = 0; enkripsi[i][0] != -1; i++) // Menggunakan en[i][0] != -1 sebagai kondisi loop
-//         printf("%c", enkripsi[i][0]);
+    for (i = 0; enkripsi[i][0] != -1; i++) // Menggunakan en[i][0] != -1 sebagai kondisi loop
+        printf("%c", enkripsi[i][0]);
 }
  
-
-
-
-//  int main ()
-//  {
-//     printf ("Angka dengan minimal diatas dari 2\n");
-//     printf ("Masukkan Angka Prima Pertama: ");
-//     scanf ("%ld",&p);
-//     cekinputprima = cekprim(p);
-//     if (cekinputprima == 0)
-//     {
-//         printf ("Input mengalami error// tidak prima");
-//         exit(1);
+// void decrypt(int enkripsi[][100],int pass[][100])
+// {
+//     int pt, ct, key = d[0][0], k;
+//     i = 0;
+//     while (enkripsi[i][j] != -1)
+//     {   
+//         ct = temp[i][j];
+//         k = 1;
+//         for (j = 0; j < key; j++)
+//         {
+//             k = k * ct;
+//             k = k % n;
+//         }
+//         pt = k + 96;
+//         pass[i][j] = pt;
+//         j++;
+//         i++;
 //     }
-//     printf ("Angka dengan minimal diatas dari 1\n");
-//     printf ("Masukkan angka Prima kedua: ");
-//     scanf ("%ld",&q);
-//     cekinputprima = cekprim(q);
-//     if (cekinputprima == 0 || p == q)
-//     {
-//         printf ("Input mengalami error/ TIdak prima");
-//         exit (1);
-//     }
-//     printf ("Masukkan Password yang akan digunakan");
-//     fflush(stdin);
-//     scanf("%s",password);
-
-
-
-
+//     pass[i][j] = - 1;
+//         for (i = 0; pass[i][0] != -1; i++) // Menggunakan en[i][0] != -1 sebagai kondisi loop
+//         printf("%c", pass[i][0]);
 // }
+
+ int main ()
+ {
+    printf ("Angka dengan minimal diatas dari 2\n");
+    printf ("Masukkan Angka Prima Pertama: ");
+    scanf ("%ld",&p);
+    cek = cekprim(p);
+    if (cek == 0)
+    {
+        printf ("Input mengalami error// tidak prima");
+        exit(1);
+    }
+    printf ("Angka dengan minimal diatas dari 1\n");
+    printf ("Masukkan angka Prima kedua: ");
+    scanf ("%ld",&q);
+    cek = cekprim(q);
+    if (cek == 0 || p == q)
+    {
+        printf ("Input mengalami error/ TIdak prima");
+        exit (1);
+    }
+    printf ("Masukkan Password yang akan digunakan : \n");
+    scanf("%s",password);
+      for (i = 0; password[i][j] != '\0'; i++)
+      {
+        for (j = 0;  j <= i; j++)
+        {
+         pass[i][j] = password[i][j];
+        }
+      }
+      mengolahtdann(p, q);
+      bagE ( t, e, d);
+      bagianD(t);
+      encrypt();
+}
 
 
--- 231511068/231511068.h
+++ 231511068/231511068.h
@@ -1,10 +1,13 @@
-#include<stdio.h>
+#include<iostream>
 #include<stdlib.h>
 #include<string.h>
 #include<math.h>
+using namespace std;
 
-
-void encrypt(long int pass[][100]);
-long int bagianD(long int x);
-void bagE(long int cekinputprima);
-int cekprim(long int prima);
+void encrypt( int pass[][100]);
+int bagianD(int t);
+int cekprim( int prima);
+void bagE (int t,int *e,int *d);
+void decrypt(int enkripsi[][100],int pass[][100]);
+int mengolahtdann(int p, int q); 
+ 
