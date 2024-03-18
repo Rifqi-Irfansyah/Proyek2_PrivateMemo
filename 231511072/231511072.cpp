83b569100b1207f17c1dbdeb3863b7122a0c485b
Berliana Novianti <berliananovianti43@gmail.com>
2024-03-18 08:30:16 +0700

    ...

--- 231511072/231511072.cpp
+++ 231511072/231511072.cpp
@@ -1,25 +1,3 @@
-//#include "231511072.h"
-// #include <fstream>
-// #include <iostream>
-// using namespace std;
-
-// void readMemoFile(const char* filename, Memo& memo) {
-    // ifstream file(filename, binary);
-    // if (!file.is_open()) {
-        // cerr << "Gagal Membuka Memo: " << filename << endl;
-        // return;
-    //}
-    // Read memo name
-    // getline(file, memo.namaMemo);
-
-    // Read date (assumed to be the next line)
-    // getline(file, memo.tanggal);
-
-    // getline(file, memo.isiMemo); 
-
-    // file.close();
-//} 
-// memo_reader.cpp
 #include <iostream>
 #include <fstream>
 #include <string>
