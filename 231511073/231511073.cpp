3126f188e96210f47f175811b90409b719aefb62
Dio-Second-Account <163722980+Dio-Second-Account@users.noreply.github.com>
2024-03-21 22:00:04 +0700

    Membuat Algoritma Enkripsi

--- 231511073/231511073.cpp
+++ 231511073/231511073.cpp
@@ -1,80 +1,22 @@
-#include"231511073.h"
+#include <iostream>
+#include <cstring>
 
-long int p; 
-long int q;
-long int n;
-long int t;
-long int flag;
-long int key[MAX_SIZE][20];
-long int temp[MAX_SIZE];
-long int m[MAX_SIZE];
-long int enkripsi[MAX_SIZE];
-char msg[MAX_SIZE];
+const int MAX_ROWS = 10;
+const int MAX_COLS = 10;
 
-int prima(long int pr) {
-    int i;
-    long int j = sqrt(pr);
-    for (i = 2; i <= j; i = i + 1) {
-        if (pr % i == 0)
-            return 0;
-    }
-    return 1;
-}
+char* encrypt(const char plainText[], int key) {
+    char* encryptedText = new char[MAX_ROWS * MAX_COLS];
+    int len = strlen(plainText);
 
-// int main-nya dipindahin ke bawah
-
-void ce() {
-    int k = 0, j = 0;
-    for (int i = 2; i < t; i++) {
-        if (t % i == 0)
-            continue;
-        flag = prima(i);
-        if (flag == 1 && i != p && i != q) {
-            key[k][0] = i;
-            if (flag > 0) {
-                key[k][1] = flag;
-                k++;
-            }
-            if (k == MAX_SIZE)
-                break;
-        }
-    }
-}
-
-// char- nya di tambahin 50
-
-string encrypt(const string& msg) {
-    char password_encrypt[50] = "";
-    p = 89;
-    flag = prima(p);
-
-    q = 97;
-    flag = prima(q);
-
-    for (int i = 0; msg[i] != '\0'; i = i +1)
-        m[i] = msg[i];
-    
-    n = p * q;
-    t = (p - 1) * (q - 1);
-    ce();
-
-    long int pt, ct, len;
-    len = msg.size(); 
     for (int i = 0; i < len; i++) {
-        pt = m[i];
-        pt = pt - 96;
-        long int k = 1;
-        for (int j = 0; j < key[0][0]; j++) {
-            k = k * pt;
-            k = k % n;
+        if (isalpha(plainText[i])) {
+            char base = islower(plainText[i]) ? 'a' : 'A';
+            encryptedText[i] = (plainText[i] - base + key) % 26 + base;
+        } else {
+            encryptedText[i] = plainText[i];
         }
-        temp[i] = k;
-        ct = k + 96;
-        enkripsi[i] = ct;
     }
-    enkripsi[len] = -1;
-    for (int i = 0; enkripsi[i] != -1; i++){
-        password_encrypt[i] = enkripsi[i];
-    }
-    return password_encrypt;
+    encryptedText[len] = '\0'; // Add null terminator
+
+    return encryptedText;
 }
