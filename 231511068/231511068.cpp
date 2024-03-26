c3cdc0d925fbbf3907e51d01a7fbb838f9b6b06b
Alqan Nazra <alqan.nazra.tif23@polban.ac.id>
2024-03-26 11:39:07 +0700

    dibuat kelebih sederhana

--- 231511068/231511068.cpp
+++ 231511068/231511068.cpp
@@ -7,15 +7,19 @@ const int MAX_COLS = 100;
 char* decrypt(const char cipherText[], int key) {
     char* decryptedText = new char[MAX_ROWS * MAX_COLS];
     int len = strlen(cipherText);
-
+    
     for (int i = 0; i < len; i++) {
-        if (isalpha(cipherText[i])) {
-            char base = islower(cipherText[i]) ? 'a' : 'A';
-            decryptedText[i] = (cipherText[i] - base - key + 26) % 26 + base;
+        char base; // Deklarasi base di dalam loop
+        if (isupper(cipherText[i])) {
+            base = 'A';
+        } else if (islower(cipherText[i])) {
+            base = 'a';
         } else {
-            decryptedText[i] = cipherText[i];
+            base = cipherText[i]; // Menyimpan karakter non-alfabet
         }
+        decryptedText[i] = (cipherText[i] - base - key + 26) % 26 + base;
     }
+
     decryptedText[len] = '\0'; // Add null terminator
 
     return decryptedText;
