a259ff66012b2eeec3a9ee8746b8d665c9961648
Alqan Nazra <alqan.nazra.tif23@polban.ac.id>
2024-03-30 09:27:44 +0700

    perbaikan

--- 231511068/231511068.cpp
+++ 231511068/231511068.cpp
@@ -7,19 +7,17 @@ const int MAX_COLS = 100;
 char* decrypt(const char cipherText[], int key) {
     char* decryptedText = new char[MAX_ROWS * MAX_COLS];
     int len = strlen(cipherText);
-    
-    for (int i = 0; i < len; i++) {
-        char base; // Deklarasi base di dalam loop
-        if (isupper(cipherText[i])) {
-            base = 'A';
-        } else if (islower(cipherText[i])) {
-            base = 'a';
-        } else {
-            base = cipherText[i]; // Menyimpan karakter non-alfabet
-        }
-        decryptedText[i] = (cipherText[i] - base - key + 26) % 26 + base;
-    }
 
+    for (int i = 0; i < len; i++) {
+        if (isalpha(cipherText[i])) {
+            char base = islower(cipherText[i]) ? 'a' : 'A'; // ini merupakan operasi tenary dimana operasi ini seperti boolean namun lebih singkat dengan 
+            /* ketentuan (condition) ? value_if_true : value_if_false sehingga dalam baris ini menjelaskan bahwa ketika operasi true maka  nilai kecil akan
+            dimasukkan pada base */ 
+            decryptedText[i] = (cipherText[i] - base - key + 26) % 26 + base;
+        } else {
+            decryptedText[i] = cipherText[i];
+        }
+    }
     decryptedText[len] = '\0'; // Add null terminator
 
     return decryptedText;
