f82e8fd8218ba280313418fdb3ab65b6885b8b4d
Berliana Novianti <berliananovianti43@gmail.com>
2024-05-09 10:28:54 +0700

    mengubah dan menyesuaikan linked list

--- 231511072/231511072.cpp
+++ 231511072/231511072.cpp
@@ -19,26 +19,66 @@ int countDataMemo(){
     return numMemo;  
 }
 
-Memo2* readMemoFile(){
-    Memo2 *readMemo = nullptr ; // Pointer ke array Memo
-    int numMemo = 0; 
+// Memo2* readMemoFile(){
+//     Memo2 *readMemo = nullptr ; // Pointer ke array Memo
+//     int numMemo = 0; 
 
-    numMemo = countDataMemo(); 
-    // Alokasikan memori untuk array readMemo
-    readMemo = new Memo2[numMemo];
+//     numMemo = countDataMemo(); 
+//     // Alokasikan memori untuk array readMemo
+//     readMemo = new Memo2[numMemo];
 
-    // Baca data sebenarnya ke dalam array readMemo
-    ifstream readFileAgain("Memos.dat", ios::in | ios::binary);
-    if (readFileAgain.is_open()) 
-    {
-        // Baca data dalam array readMemo yg telah dialokasi
-        readFileAgain.read(reinterpret_cast<char*>(readMemo), numMemo * sizeof(Memo2));
-        readFileAgain.close();
-    } 
-    else 
-    {
-        cerr << "Gagal membuka file" << endl;
+//     // Baca data sebenarnya ke dalam array readMemo
+//     ifstream readFileAgain("Memos.dat", ios::in | ios::binary);
+//     if (readFileAgain.is_open()) 
+//     {
+//         // Baca data dalam array readMemo yg telah dialokasi
+//         readFileAgain.read(reinterpret_cast<char*>(readMemo), numMemo * sizeof(Memo2));
+//         readFileAgain.close();
+//     } 
+//     else 
+//     {
+//         cerr << "Gagal membuka file" << endl;
+//     }
+
+//     return readMemo;
+// }
+int readRecords(const char* filename, address_memo& awal, address_memo& akhir) {
+    ifstream inFile(filename, ios::binary);
+    if (!inFile) {
+        awal = nullptr;
+        akhir = nullptr;
+        // ofstream outFile(filename, std::ios::binary);
+        return 0;
     }
 
-    return readMemo;
+    // Initialize head pointer
+    awal = nullptr;
+    akhir = nullptr;
+    address_memo current = nullptr;
+    while (inFile.peek() != EOF) {
+
+        // Create a new Memo object
+        address_memo newNode = new ElmtList;
+
+        // Read data from file into the new Memo object
+        inFile.read(reinterpret_cast<char*>(&newNode->id_memo), sizeof(int));
+        inFile.read(reinterpret_cast<char*>(&newNode->namaMemo), sizeof(char[30]));
+        inFile.read(reinterpret_cast<char*>(&newNode->isiMemo), sizeof(char[300]));
+        inFile.read(reinterpret_cast<char*>(&newNode->password), sizeof(char[50]));
+        inFile.read(reinterpret_cast<char*>(&newNode->tanggal), sizeof(time_t));
+
+        // Link the new node to the linked list
+        if (!awal) {
+            awal = newNode;
+        } else {
+            current->next = newNode;
+            // newNode->prev = current;
+        }
+        current = newNode;
+        akhir = current;
+        current->next = NULL;
+    }
+
+    inFile.close();
+    return akhir->id_memo;
 }
--- 231511072/231511072.h
+++ 231511072/231511072.h
@@ -3,19 +3,27 @@
 
 #include <string>
 #include <ctime>
+// #include "../231511095/231511095.h"
 using namespace std;
 
-struct Memo2 
-{
-    char namaMemo[30]; 
-    char isiMemo[300]; 
-    char password[50]; 
-    time_t tanggal; 
+typedef struct Memo *address_memo;
+typedef struct Memo {
+    int id_memo; //var untuk menyimpan id memo
+    char namaMemo[30]; // subvar namaMemo bertipe char dengan length 30
+    char isiMemo[300]; // subvar isiMemo bertipe char dengan length 300
+    char password[50]; // subvar password bertipe char dengan length 50
+    time_t tanggal; // subvar tanggal
+    address_memo next; //pointer ke memo berikutnya
+    address_memo prev; //pointer ke memo sebelumnya
 
-    Memo2() : namaMemo(""), isiMemo(""), password(""), tanggal(0) {} 
-};
+    Memo() : id_memo(0), namaMemo(""), isiMemo(""), password(""), tanggal(0) {}
+    // inisiasi Memo yang memiliki nilai default dengan string kosong
 
-Memo2* readMemoFile(); //membaca data memo dari sebuah file dan mengembalikan pointer ke array Memo2
+} ElmtList;
+
 int countDataMemo(); //menghitung jumlah data memo yang terdapat dalam file
+int readRecords(const char* filename, address_memo& awal, address_memo& akhir);
+
+#include "231511072.cpp"
 
 #endif // MEMO_READER_H
