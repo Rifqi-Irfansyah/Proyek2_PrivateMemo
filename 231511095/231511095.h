19a2efca6b4a8e4a6c414e8b5bbb8547e41e21c9
syahlasals <syahlasbl28@gmail.com>
2024-05-09 21:31:18 +0700

    Mengubah modul memo pada header

--- 231511095/231511095.h
+++ 231511095/231511095.h
@@ -3,21 +3,31 @@
 
 #include <string>
 #include <ctime>
+// #include "../231511089/231511089.h"
+#include "../231511072/231511072.h"
+
 using namespace std;
 
 // deklarasi Struct Memo yang berisi subvar namaMemo, isiMemo, password
-struct Memo {
-    char namaMemo[30]; // subvar namaMemo bertipe char dengan length 30
-    char isiMemo[300]; // subvar isiMemo bertipe char dengan length 300
-    char password[50]; // subvar password bertipe char dengan length 50
-    time_t tanggal; // subvar tanggal
+// typedef struct Memo *address_memo;
+// typedef struct Memo {
+//     int id_memo;
+//     char namaMemo[30]; // subvar namaMemo bertipe char dengan length 30
+//     char isiMemo[300]; // subvar isiMemo bertipe char dengan length 300
+//     char password[50]; // subvar password bertipe char dengan length 50
+//     time_t tanggal; // subvar tanggal
+//     address_memo next;
+//     address_memo prev;
 
-    Memo() : namaMemo(""), isiMemo(""), password(""), tanggal(0) {}
-    // inisiasi Memo yang memiliki nilai default dengan string kosong
+//     Memo() : namaMemo(""), isiMemo(""), password(""), tanggal(0) {}
+//     // inisiasi Memo yang memiliki nilai default dengan string kosong
 
-};
+// } ElmtList;
 
 void tambahMemo(const Memo& memo); // deklarasi modul tambahMemo
 void inputMemo(); // deklarasi modul inputMemo
+void saveRecords(const char* filename, address_memo awal);
+
+#include "231511095.cpp"
 
 #endif // MEMO_H
