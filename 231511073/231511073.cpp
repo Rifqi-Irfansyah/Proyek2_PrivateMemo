7d7d4416e0d21b1e0c34e1ef20d16d01c90d78d0
Dio Second Account <163722980+Dio-Second-Account@users.noreply.github.com>
2024-05-17 11:16:06 +0700

    Memperbaiki Spasi
    Mengubah apabila ada spasi/space " " menjadi simbol persen "%"

--- 231511073/231511073.cpp
+++ 231511073/231511073.cpp
@@ -3,15 +3,12 @@
 #include <cctype>
 
 using namespace std;
-
 const int MAX_COLS = 100;
-
 
 struct Node {
     char data;
     Node* next;
 };
-
 
 class Queue {
     private:
@@ -59,7 +56,6 @@ class Queue {
         }
 };
 
-
 char* encrypt(const char plainText[], int key) {
     Queue queue;
 
@@ -67,7 +63,9 @@ char* encrypt(const char plainText[], int key) {
     char* encryptedText = new char[len + 1]; // +1 untuk null terminator
 
     for (int i = 0; i < len; i++) {
-        if (isalpha(plainText[i])) {
+        if (plainText[i] == ' ') {
+            queue.enqueue('%'); // Ubah spasi menjadi %
+        } else if (isalpha(plainText[i])) {
             char base = islower(plainText[i]) ? 'a' : 'A';
             char encryptedChar = (plainText[i] - base + key) % 26 + base;
             queue.enqueue(encryptedChar);
@@ -89,20 +87,130 @@ int main() {
     char plainText[MAX_COLS];
     int key;
 
-    cout << "Masukan Nama : ";
+    cout << "Masukkan Nama-mu : ";
     cin.getline(plainText, MAX_COLS);
 
-    cout << "Masukan Usia : ";
+    cout << "Masukkan Usia-mu : ";
     cin >> key;
 
     char* encryptedText = encrypt(plainText, key);
-    cout << "\nTeks Ter-enkrip: " << encryptedText << endl;
+    cout << "\nTeks Ter-enkrip : " << encryptedText << endl;
 
     delete[] encryptedText;
 
     return 0;
 }
 
+
+// kodingan kedua
+
+// #include <iostream>
+// #include <cstring>
+// #include <cctype>
+
+// using namespace std;
+
+// const int MAX_COLS = 100;
+
+
+// struct Node {
+    // char data;
+    // Node* next;
+// };
+
+
+// class Queue {
+    // private:
+        // Node* front;
+        // Node* rear;
+    // public:
+        // Queue() : front(nullptr), rear(nullptr) {}
+        // ~Queue() {
+            // while (!isEmpty()) {
+                // dequeue();
+            // }
+        // }
+
+        // bool isEmpty() {
+            // return front == nullptr;
+        // }
+
+        // void enqueue(char value) {
+            // Node* newNode = new Node;
+            // newNode->data = value;
+            // newNode->next = nullptr;
+
+            // if (isEmpty()) {
+                // front = newNode;
+            // } else {
+                // rear->next = newNode;
+            // }
+            // rear = newNode;
+        // }
+
+        // char dequeue() {
+            // if (isEmpty()) {
+                // cerr << "Queue Kosong" << endl;
+                // return '\0';
+            // }
+
+            // Node* temp = front;
+            // char value = front->data;
+            // front = front->next;
+            // delete temp;
+            // if (front == nullptr) {
+                // rear = nullptr;
+            // }
+            // return value;
+        // }
+// };
+
+
+// char* encrypt(const char plainText[], int key) {
+    // Queue queue;
+
+    // int len = strlen(plainText);
+    // char* encryptedText = new char[len + 1]; // +1 untuk null terminator
+
+    // for (int i = 0; i < len; i++) {
+       // if (isalpha(plainText[i])) {
+           // char base = islower(plainText[i]) ? 'a' : 'A';
+           // char encryptedChar = (plainText[i] - base + key) % 26 + base;
+           // queue.enqueue(encryptedChar);
+       // } else {
+           // queue.enqueue(plainText[i]);
+       // }
+   // }
+
+   // int index = 0;
+   // while (!queue.isEmpty()) {
+       // encryptedText[index++] = queue.dequeue();
+   // }
+   // encryptedText[index] = '\0';
+
+   // return encryptedText;
+// }
+
+// int main() {
+    // char plainText[MAX_COLS];
+    // int key;
+
+    // cout << "Masukan Nama : ";
+    // cin.getline(plainText, MAX_COLS);
+
+    // cout << "Masukan Usia : ";
+    // cin >> key;
+
+    // char* encryptedText = encrypt(plainText, key);
+    // cout << "\nTeks Ter-enkrip: " << encryptedText << endl;
+
+    // delete[] encryptedText;
+
+    // return 0;
+// }
+
+// kodingan pertama
+    
 // #include <iostream>
 // #include <cstring>
 
