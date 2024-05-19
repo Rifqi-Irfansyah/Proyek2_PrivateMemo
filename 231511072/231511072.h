f8cdd86bd76aa651089fe9165d723484548789b9
Berliana Novianti <berliananovianti43@gmail.com>
2024-05-19 23:11:28 +0700

    menambahkan var key

--- 231511072/231511072.h
+++ 231511072/231511072.h
@@ -9,6 +9,7 @@ using namespace std;
 typedef struct Memo *address_memo;
 typedef struct Memo {
     int id_memo; //var untuk menyimpan id memo
+    int key; //var untuk menyimpan key memo
     char namaMemo[30]; // subvar namaMemo bertipe char dengan length 30
     char isiMemo[300]; // subvar isiMemo bertipe char dengan length 300
     char password[50]; // subvar password bertipe char dengan length 50
@@ -16,14 +17,14 @@ typedef struct Memo {
     address_memo next; //pointer ke memo berikutnya
     address_memo prev; //pointer ke memo sebelumnya
 
-    Memo() : id_memo(0), namaMemo(""), isiMemo(""), password(""), tanggal(0) {}
+    Memo() : id_memo(0), namaMemo(""), isiMemo(""), password(""), tanggal(0), next(NULL), prev(NULL) {}
     // inisiasi Memo yang memiliki nilai default dengan string kosong
 
 } ElmtList;
 
 // int countDataMemo(); //menghitung jumlah data memo yang terdapat dalam file
 int readRecords(const char* filename, address_memo& awal, address_memo& akhir);
- 
+  
 #include "231511072.cpp"
 
 #endif // MEMO_READER_H
