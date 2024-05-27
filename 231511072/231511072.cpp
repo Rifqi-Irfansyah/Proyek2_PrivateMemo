3a5585002e6e0d0d000f13562b34a5f7ce169c78
Berliana Novianti <berliananovianti43@gmail.com>
2024-05-27 08:58:11 +0700

    menghilangkan komentar tidak perlu pada file cpp

--- 231511072/231511072.cpp
+++ 231511072/231511072.cpp
@@ -3,45 +3,6 @@
 #include <string>
 #include "231511072.h"
 
-// int countDataMemo(){  
-//     int numMemo = 0; // Jumlah data Memo yang terbaca
-//     ifstream readFile("Memos.dat", ios::in | ios::binary); //membuka file Memos.dat dalam mode baca dan mode biner
-//     if (readFile.is_open()) /
-//     {
-//         // Hitung jumlah data dengan membaca per satu item
-//         Memo2 temp; //membaca data dari file sementara
-//         while (readFile.read(reinterpret_cast<char*>(&temp), sizeof(Memo2))) //perulangan while yang akan membaca data dari file Memos.dat ke dalam objek temp
-//         {
-//             numMemo++; //menambahkan nilai numMemo dengan 1 setiap kali objek temp berhasil dibaca dari file //menghitung jumlah data Memo2 yang terbaca
-//         }
-//         readFile.close();
-//     }
-//     return numMemo;  
-// }
-
-// Memo2* readMemoFile(){
-//     Memo2 *readMemo = nullptr ; // Pointer ke array Memo
-//     int numMemo = 0; 
-
-//     numMemo = countDataMemo(); 
-//     // Alokasikan memori untuk array readMemo
-//     readMemo = new Memo2[numMemo];
-
-//     // Baca data sebenarnya ke dalam array readMemo
-//     ifstream readFileAgain("Memos.dat", ios::in | ios::binary);
-//     if (readFileAgain.is_open()) 
-//     {
-//         // Baca data dalam array readMemo yg telah dialokasi
-//         readFileAgain.read(reinterpret_cast<char*>(readMemo), numMemo * sizeof(Memo2));
-//         readFileAgain.close();
-//     } 
-//     else 
-//     {
-//         cerr << "Gagal membuka file" << endl;
-//     }
-
-//     return readMemo;
-// }
 int readRecords(const char* filename, address_memo& awal, address_memo& akhir) {
     ifstream inFile(filename, ios::binary);
     if (!inFile) {
@@ -81,4 +42,4 @@ int readRecords(const char* filename, address_memo& awal, address_memo& akhir) {
 
     inFile.close();
     return akhir->id_memo;
-}
+} 
